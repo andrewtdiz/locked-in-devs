d040f8451972f4d5c1b5d55d934d3a7b590a1108
andrewtdiz <59515127+andrewtdiz@users.noreply.github.com>
2024-11-10 12:43:35 -0600

    v1.02

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -4,7 +4,7 @@ import { CommandInteraction } from "discord.js";
 export const pingCommand = {
   data: new SlashCommandBuilder()
     .setName("ping")
-    .setDescription("Replies with Pong! (v1.01)"),
+    .setDescription("Replies with Pong! (v1.02)"),
   async execute(interaction: CommandInteraction) {
     await interaction.reply("Pong!");
   },
