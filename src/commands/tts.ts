b79fd718d774715ea79f520809825ab6c11666de
andrewtdiz <59515127+andrewtdiz@users.noreply.github.com>
2024-12-18 12:51:21 -0600

    revert

--- src/commands/tts.ts
+++ src/commands/tts.ts
@@ -8,25 +8,13 @@ import ttsRole from "../constants/ttsRole";
 
 export default {
   data: new SlashCommandBuilder()
-    .setName("toggletts")
+    .setName("tts")
     .setDescription("Toggles TTS for the current user"),
 
   async execute(interaction: CommandInteraction) {
     await interaction.deferReply();
 
     const member = interaction.member as GuildMember;
-    const userVoiceState = member.voice.channel;
-    const isServerMuted = member.voice.serverMute;
-
-    if (!userVoiceState || isServerMuted) {
-      if (userVoiceState) {
-        return interaction.editReply(
-          "Cannot toggle TTS, user is not in a voice channel"
-        );
-      }
-      return interaction.editReply("Cannot toggle TTS, user is muted");
-    }
-
     const userHasToggleRole = member.roles.cache.has(ttsToggleRole);
 
     if (userHasToggleRole) {
--- src/index.ts
+++ src/index.ts
@@ -54,7 +54,6 @@ const musicBotCommands = [
 
 const commands = [
   ttsCommand,
-  randomCommand,
   // lockInCommand,
   pingCommand,
   // unlockCommand,
